edd71fa1e7d3c7152e54d5ea694a56b692e2b692
avi <hi@avi.im>
2020-05-06 13:03:47 +0530

    Add comments for types

--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package hedwig
 
-
 const (
-	ExchangeDelayed  = "x-delayed-message"
+	// For exchanges which use RMQ delay plugin, following type should be used as ExchangeType
+	ExchangeTypeDelayed = "x-delayed-message"
+	// Delayed Type Exchanges also need an extra arg with following key with the value set to actual ExchangeType
+	// like `direct`, `topic` etc
+	DelayedExchangeArgKey = "x-delayed-type"
 )
